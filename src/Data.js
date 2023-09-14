af1dbaf74ab9598e7a9416f21cbd8d0b2034eea3
VishnuMajeti <139220768+VishnuMajeti@users.noreply.github.com>
2023-09-14 17:28:39 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,110 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Almond WalNut",
+    price: "5.2",
+    desc: "Delicious blend of almonds, walnuts, and rich chocolate.",
+    img: "https://img.freepik.com/free-vector/realistic-almond-walnut-dark-chocolate-bar-package-template-vector-illustration_1284-68284.jpg?w=740&t=st=1694685712~exp=1694686312~hmac=5ff5c0959f794d0ec797ab14c4586fdc1d7fcb6563cc004586945ab24977296f",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Milky Chocolate",
+    price: "3.5",
+    desc: "Creamy, smooth, and sweet indulgence in every bite.",
+    img: "https://img.freepik.com/free-vector/collection-chocolate-bars-pieces_23-2147810332.jpg?w=740&t=st=1694686696~exp=1694687296~hmac=5612f2e313db70173855be1ecf8b9e72467addd04c8f3410c03b34948f321a5e",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "KitKat",
+    price: "1.9",
+    desc: "Crunchy, iconic, and irresistible chocolate wafer treat.",
+    img: "https://img.freepik.com/free-vector/collection-chocolate-bars_23-2147819874.jpg?w=740&t=st=1694686967~exp=1694687567~hmac=6114ab361e328e8cbf2f75560fbf714f9ed7c7d3a3f45e2d35b78bedbf5f4adc",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Milk Splash",
+    price: "2.4",
+    desc: "Creamy, luscious, and refreshing milk-infused delight.",
+    img: "https://img.freepik.com/free-vector/watercolor-banners-milk-splash-with-brown-background_23-2147591365.jpg?w=740&t=st=1694687240~exp=1694687840~hmac=9271abe5d1c5e6f8b1c1e8270816a461847ef9cdf5217d5440c8a1f313d4eb2b",
+  },
+
+  {
+    id: "cbscscjhsbj",
+    name: "Delicious Chocolate",
+    price: "1.7",
+    desc: "Delectable, heavenly, and satisfying cocoa-rich treat.",
+    img: "https://img.freepik.com/free-vector/delicious-chocolate-bars-collection_23-2147823199.jpg?w=740&t=st=1694687436~exp=1694688036~hmac=4295391f1c0cf141d54d930982495784b9b0d80d5fbd6b8aed2c5eef033d5d3c",
+  },
+
+  {
+    id: "nvbvbvbhssb",
+    name: "Dark Fantasy",
+    price: "3.5",
+    desc: "Mysterious, decadent, and seductive chocolate experience.",
+    img: "https://img.freepik.com/free-vector/set-bars-pieces-with-different-chocolates_23-2147815799.jpg?w=740&t=st=1694687992~exp=1694688592~hmac=f3ae35a84071643ba110d45bdfde45203e43851821c961becf715813913488bc",
+  },
+
+  {
+    id: "cnvbbhsbshb",
+    name: "Huzzle Nuts",
+    price: "1.3",
+    desc: "Crunchy nuts enveloped in velvety, decadent chocolate.",
+    img: "https://img.freepik.com/free-vector/collection-chocolate-bars-realistic-style_23-2147813917.jpg?w=740&t=st=1694688750~exp=1694689350~hmac=bf1a61f8a32fbdd49de1e42a94617fc90e7483071fd14b1253b82b558641250a",
+  },
+
+  {
+    id: "nmvmnbvsbmvu",
+    name: "Minty fresh chocos",
+    price: "2.8",
+    desc: "Cool, refreshing mint-infused chocolate delight with a crunch.",
+    img: "https://img.freepik.com/free-vector/banner-template-with-chocolate-winter_83728-5438.jpg?w=740&t=st=1694688527~exp=1694689127~hmac=fdf4dd0f6013819332a171f8f77b15f91f757b726d98b5a3ba138a750a0ea753",
+  },
+ 
+  {
+    id: "nvvbhjvbjvvxc",
+    name: "Hersheys",
+    price: "5.4",
+    desc: "Iconic American milk chocolate loved worldwide for decades.",
+    img: "https://img.freepik.com/free-vector/delicious-snack-collection-with-realistic-design_23-2147909595.jpg?w=740&t=st=1694690798~exp=1694691398~hmac=8d0e6804350cbfca53501ba49705aabf6b5420736e896d725c39b2c948f2953a",
+  },
+
+  {
+    id: "cnvbdbdbjnkj",
+    name: "chocolate crunch",
+    price: "6.2",
+    desc: "Irresistible combination of crispy and smooth chocolate.",
+    img: "https://img.freepik.com/premium-photo/premium-packaging-professional-set-elevate-your-product-designs-with-realistic-precision-bag-box_655090-105646.jpg?w=740",
+  },
+   
+  {
+    id: "jkjkjdvmbvbm",
+    name: "Authentic Chocolate",
+    price: "5.6",
+    desc: "Pure, genuine, and delectable cocoa confection.",
+    img: "https://img.freepik.com/free-vector/delicious-chocolate-bars-collection_23-2147823200.jpg?w=740&t=st=1694690883~exp=1694691483~hmac=6a5b4c3d0e7e378e972e7cbb19c5329f32711fb1a2149560b2952be8d5f419d0",
+  },
+
+  {
+    id: "ndmdmdjjdbkjd",
+    name: "Choco Bite",
+    price: "2.8",
+    desc: "Sweet, bite-sized chocolate pleasure in every piece.",
+    img: "https://img.freepik.com/free-psd/vertical-poster-chocolate_23-2148855194.jpg?w=740&t=st=1694690934~exp=1694691534~hmac=43cf2ad91cf40776802a775f00b7cac7bd5da0094f3b2cf66c115be3019a9500",
+  },
+   
+  {
+    id: "tgnmdfbmdbnc",
+    name: "Autumn Chocolate",
+    price: "6.7",
+    desc: "Rich, warm, and seasonal chocolate delight.",
+    img: "https://img.freepik.com/free-vector/chocolate-bar-concept-illustration_114360-14633.jpg?w=740&t=st=1694691060~exp=1694691660~hmac=60a79aa62481018ab36ef1ade748be61191f1d46d21d45cc1e6ebe423bc97deb",
+  },
+
+  {
+    id: "vxbvmxbvmvbmv",
+    name: "Delight packs",
+    price: "10",
+    desc: "Joyful, assorted bundles of tasty chocolate treats.",
+    img: "https://img.freepik.com/premium-vector/snack-set-chocolate-fast-food-products-sweet-desert-candies-with-nut-vending-machine-food-store-elements-lunch-box-market-design-cartoon-style-vector_253363-290.jpg?w=740",
+  },
+];
